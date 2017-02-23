d3d9588fac4e936bfbb483aca788169b0ea0f4ee
Student <student@macbook-pro-1174-corley.local>
2017-02-23 18:10:57 -0500

    finished my example for conditionals

--- ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding/ShowingMyUnderstanding.playground/Pages/Conditionals - Create an Example.xcplaygroundpage/Contents.swift
@@ -20,7 +20,28 @@ import PlaygroundSupport
 let canvas = Canvas(width: 300, height: 300)
 
 // Replace this command and add your code below...
+let colour = random(from: 0, toButNotIncluding: 7)
 
+
+if colour == 1 {
+    canvas.fillColor = Color.green
+} else if colour == 2{
+    canvas.fillColor = Color.red
+} else if colour == 3{
+    canvas.fillColor = Color.purple
+} else if colour == 4{
+    canvas.fillColor = Color.yellow
+} else if colour == 5{
+    canvas.fillColor = Color.orange
+} else if colour == 6{
+    canvas.fillColor = Color.blue
+}
+
+
+canvas.drawEllipse(centreX: 150, centreY: 150, width: 100, height: 100, borderWidth: 10)
+
+canvas.drawEllipse(centreX: 125, centreY: 155, width: 10, height: 10, borderWidth: 5)
+canvas.drawEllipse(centreX: 175, centreY: 155, width: 10, height: 10, borderWidth: 5)
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
